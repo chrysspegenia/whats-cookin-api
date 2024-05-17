7911d4662f8332a04122ae3a697fcc0575f10120
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-05-17 20:39:58 +0800

    Create recipes table

---
+++ db/migrate/20240516113356_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title, null: false
+      t.string :description, null: false
+      t.text :instructions, null: false
+      t.string :image_url, null: false
+      t.string :public_code, null: false
+      t.timestamps null: false
+    end
+
+    add_reference :recipes, :user, null: false, foreign_key: true
+  end
+end
